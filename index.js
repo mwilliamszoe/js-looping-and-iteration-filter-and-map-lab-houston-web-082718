9f5853f635bb100a404203ed16f030ad58be60ee
Zoe Monet Williams <mwilliamszoe@gmail.com>
2020-02-25 14:49:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-// Code your solution here:
-function driversWithRevenueOver (drivers, revenue) {
-    return drivers.filter(function (driver) {
-      return driver.revenue > revenue;
+// * `exactMatch()` - This function takes an array of `driver` objects as the first argument and a JavaScript object that specifies an attribute and corresponding value. For example, `exactMatch(drivers, { revenue: 3000 })` will return all drivers whose `revenue` attribute equals `3000`, and `exactMatch(drivers, { name: 'Bob' })` will return all drivers whose `name` attribute equals `Bob`.
+// * `exactMatchToList()` - This function works the same way as the `exactMatch()` function, except that instead of returning an array of `driver` objects, it returns an array of strings with each element in the array corresponding to the matching driver's name. For example, `exactMatchToList(drivers, { revenue: 2000 })` will return `['Sammy']`, as `Sammy` is the name of the matching `driver` object.
+
+function driversWithRevenueOver (driversArr, revenueInt) {
+    return driversArr.filter(function (driver) {
+      return driver.revenue > revenueInt;
     });
   }
-  
-  function driverNamesWithRevenueOver (drivers, revenue) {
-    return driversWithRevenueOver(drivers, revenue)
-      .map(function (driver) {
-        return driver.name;
-      });
-  }
-  
-  function exactMatch (drivers, matcher) {
-    return drivers.filter(function (driver) {
-      let matches = false;
-  
-      for (const key in matcher) {
-        matches = driver[key] === matcher[key];
-      }
-  
-      return matches;
-    });
-  }
-  
-  function exactMatchToList (drivers, matcher) {
-    return exactMatch(drivers, matcher)
-      .map(function (driver) {
-        return driver.name;
-      });
-  }
+
+function driverNamesWithRevenueOver(driversArr, revenueInt){
+  retrun driversWithRevenueOver(driversArr, revenueInt).map(function(driver){
+    return driver.name;
+  });
+}
+
